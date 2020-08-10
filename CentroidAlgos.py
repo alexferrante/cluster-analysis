750d2be3769dea7702884b427aa95eb204110ba3
alexferrante <36389437+alexferrante@users.noreply.github.com>
2020-08-10 13:55:23 -0700

    Add more verbose, complete versions of k/k++

--- CentroidAlgos.py
+++ CentroidAlgos.py
@@ -8,39 +8,72 @@ class CentroidAlgos():
     MAX_ITER = 300
 
 
-    def kmeans_with_auto_elbow(data):
+    def kmeans_with_auto_elbow(self, ata):
         #
 
-
-    def kmeans(data, k, centroid_opt):
+    def kmeans_plus_plus(self, data, k):
         if k == -1:
             k = kmeans_with_auto_elbow(data)
-        if centroid_opt == "rnd":
-            centroids = kmeans_rnd_centroids(data, k)
-        elif centroid_opt == "++":
-            centroids = kmeans_plus_centroids(data, k)
-        n_iter = 0
+
+        centroids = kmeans_pp_init_centroids(data, k)
         prev_centroids = np.empty(shape=[0, data.shape[1]])
+        cluster_assignment = []
+
+        n_iter = 0
         while not kmeans_terminate(prev_centroids, centroids, n_iter):
             n_iter += 1
+
+            # Assignment step: assign each data point to the closest centroid
             for i in range(len(data)):
                 distances = eucl_dist(data[i], centroids)
-                cluster[i] = np.argmin(distances)
+                cluster_assignment[i] = np.argmin(distances)
+            
             prev_centroids = np.copy(centroids)
+
+            # Centroid update step: update centroids via the average value of points assigned to it
             for i in range(k):
-                points = [data[j] for j in range(len(data)) if cluster[j] == i]
-                if points:
-                    centroid[i] = np.mean(points, axis=0)
-        return cluster
+                assigned_points = [data[j] for j in range(len(data)) if cluster[j] == i]
+                if assigned_points:
+                    centroids[i] = np.mean(assigned_points, axis=0)
+        
+        return cluster_assignment
 
 
-    def kmeans_terminate(prev_centroids, curr_centroids, curr_iter):
+    def kmeans(self, data, k):
+        if k == -1:
+            k = kmeans_with_auto_elbow(data)
+
+        centroids = kmeans_init_centroids(data, k)
+        prev_centroids = np.empty(shape=[0, data.shape[1]])
+        cluster_assignment = []
+
+        n_iter = 0
+        while not kmeans_terminate(prev_centroids, centroids, n_iter):
+            n_iter += 1
+
+            # Assignment step: assign each data point to the closest centroid
+            for i in range(len(data)):
+                distances = eucl_dist(data[i], centroids)
+                cluster_assignment[i] = np.argmin(distances)
+
+            prev_centroids = np.copy(centroids)
+
+            # Centroid update step: update centroids via the average value of points assigned to it
+            for i in range(k):
+                assigned_points = [data[j] for j in range(len(data)) if cluster[j] == i]
+                if assigned_points:
+                    centroids[i] = np.mean(assigned_points, axis=0)
+
+        return cluster_assignment
+
+
+    def kmeans_terminate(self, prev_centroids, curr_centroids, curr_iter):
         if curr_iter > MAX_ITER:
             return True
         return np.array_equal(prev_centroids, curr_centroids)
 
 
-    def kmeans_rnd_centroids(data, k, rnd_num=56):
+    def kmeans_init_centroids(self, data, k, rnd_num=56):
         np.random.seed(rnd_num)
         centroids = []
         m = np.shape(data)[0]
@@ -50,18 +83,14 @@ class CentroidAlgos():
         return np.array(centroids)
 
 
-    def kmeans_plus_centroids(data, k, rnd_num=56):
-        np.random.seed(rnd_num)
+    def kmeans_pp_init_centroids(self, data, k):
         centroids = []
         centroids.append(choice(data))
-        for _ in range(1, k):
-            all_sq_dist = np.array([min([np.inner(c - x, c - x) for c in centroids]) for x in data])
-            prob = all_sq_dist / all_sq_dist.sum()
-            cum_prob = prob.cumsum()
-            rnd = np.random.rand()
-            for cent, p in enumerate(cum_prob):
-                if rnd_num < p:
-                    i = j
-                    break
-            centroids.append(data[i])
+        for _ in range(k - 1):
+            sq_dists = np.array([min([np.square(eucl_dist(x, c)) for c in centroids]) for x in data])
+            probs = sq_dists / sq_dists.sum()
+            cum_probs = probs.cumsum()
+            rnd = np.random.random()
+            selected_cluster = np.where(cum_probs >= rnd)[0][0]
+            centroids.append(selected_cluster)
         return np.array(centroids)
